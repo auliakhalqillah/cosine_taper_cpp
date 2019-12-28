b8b93ff71ee95e622f0dff095b85b6057338b27b
Aulia Khalqillah <52442929+auliakhalqillah@users.noreply.github.com>
2019-12-28 13:21:45 +0700

    Add files via upload

---
+++ coswin_vector.cpp
@@ -0,0 +1,87 @@
+/*******************************************************************************
+Name        : coswin_vector.cpp
+Info        : Cosine Tapering
+Written by  : Aulia Khalqillah,S.Si (2019)
+Email       : auliakhalqillah.mail@gmail.com
+Last Update : December 26, 2019
+Source      : Marco Pilz adn Stefano PArolai, GFZ Postdam
+              Tapering of windowed time series
+              DOI: 10.2312/GFZ.NMSOP-2_IS_14.1
+*******************************************************************************/
+#include <iostream>
+#include <vector>
+#include <cmath>
+#include <fstream>
+using namespace std;
+int main() {
+  double a, dt, f, t1, t2, pi;
+  double x,time,c1,c2,c3,sigcom;
+  vector<double> sig,t,c,windowed;
+  int ii,i,j,k,kk,l,ll,m,n;
+  ofstream signal, winout, combine;
+  pi = 3.14;
+  a = 0.5;
+  dt = 0.0001;
+  f = 20;
+  t1 = 0.0;
+  t2 = 1.0;
+  n = (t2-t1)/dt;
+  if (a == dt){
+    cout<<"Sampling rate have to smaller than taper ratio!"<<"\n";
+  }
+  // Generate signal
+  i = 0;
+  time = 0.0;
+  signal.open("coswin_signal.txt");
+  while (i <= n) {
+    x = sin(2*pi*f*time);
+    sig.push_back(x);
+    t.push_back(time);
+    time = time + dt;
+    signal<<i<<"\t"<<t[i]<<"\t"<<sig[i]<<"\n";
+    ++i;
+  }
+  signal.close();
+
+  // Cosine taper
+  winout.open("coswinout.txt");
+  // Section 1
+  j = 0;
+  while (t[j] >= 0.0 && t[j] <= a) {
+    c1 = (1-cos(pi/a*t[j]))/2;
+    c.push_back(c1);
+    ++j;
+  }
+  // Section 2
+  k = j;
+  while (t[k] >= a && t[k] <= (1-a)) {
+    c2 = 1.0;
+    c.push_back(c2);
+    ++k;
+  }
+  // Section 3
+  l = k;
+  while (l < n){
+    c3 = (1-cos(pi/a*(1-t[l])))/2;
+    c.push_back(c3);
+    ++l;
+  }
+  m = 0;
+  while (m <= n) {
+    winout<<l<<"\t"<<t[m]<<"\t"<<c[m]<<"\n";
+    ++m;
+  }
+  winout.close();
+
+  // Combine
+  ii = 0;
+  combine.open("coswin_combined.txt");
+  while (ii < n) {
+    sigcom = sig[ii]*c[ii];
+    windowed.push_back(sigcom);
+    combine<<ii<<"\t"<<t[ii]<<"\t"<<windowed[ii]<<"\n";
+    ++ii;
+  }
+  combine.close();
+  return 0;
+}
